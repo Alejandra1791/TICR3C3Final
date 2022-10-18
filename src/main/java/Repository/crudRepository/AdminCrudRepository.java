bb6852b5a81c5e575b00172a941145eff0bfddee
Alejandra1791 <114197108+Alejandra1791@users.noreply.github.com>
2022-10-17 20:59:26 -0500

    Actualizacion admin

--- src/main/java/Repository/crudRepository/AdminCrudRepository.java
+++ src/main/java/Repository/crudRepository/AdminCrudRepository.java
@@ -1,4 +1,7 @@
 package Repository.crudRepository;
 
-public interface AdminCrudRepository {
+import Entities.Admin;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdminCrudRepository extends CrudRepository<Admin, Integer> {
 }
